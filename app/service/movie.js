939ceba7325441fb3f0453abba478da47d94c528
Lei Wang <34334180+NjtechPrinceling@users.noreply.github.com>
2020-10-21 11:41:10 +0800

    genre done

--- app/service/movie.js
+++ app/service/movie.js
@@ -13,17 +13,64 @@ class MovieService extends Service {
 
     async get_movie_detail(html) {
         const cheerioModel = cheerio.load(html);
+        // Parse Banner Info
+        // Parse Media Info
         const mediaInfo = cheerioModel('#info')[0];
-        const directors = await this.service.movie.parse_directors(mediaInfo);
-        const actors = await this.service.movie.parse_actors(mediaInfo);
-        // console.log(directors)
-        return directors
-        // console.log(cheerioModel('#info')[0].children[1].children[2].children[0].children[0].data);
+        await this.service.movie.parse_media_info_index(cheerioModel);
+        // const directors = await this.service.movie.parse_directors(mediaInfo);
+        // const actors = await this.service.movie.parse_actors(mediaInfo);
+        return {
+            // directors,
+            // actors,
+        }
+    }
+
+    // Parse directors、Actors、genre... span index
+    async parse_media_info_index(cheerioModel) {
+        let directorsIndex = null, scenaristsIndex = null, actorsIndex = null;
+        let genreIndex = []
+        const mediaInfo = cheerioModel('#info')[0];
+        for (let index = 0; index < mediaInfo.children.length; index++) {
+            const element = mediaInfo.children[index];
+            if (element.type == 'tag' && element.name == 'span') {
+                // search for director index
+                try {
+                    if (element.children[0].children[0].data == '导演') {
+                        directorsIndex = index;
+                    } else if (element.children[0].children[0].data == '编剧') {
+                        scenaristsIndex = index;
+                    } else if (element.attribs.class == 'actor') {
+                        actorsIndex = index;
+                    }
+
+                } catch (error) { }
+                // search for genre index array
+                try {
+                    if (element.attribs.property) {
+                        genreIndex.push(index);
+                        console.log(index)
+                    }
+                } catch (error) { }
+
+                // console.log(element.attribs.property)
+            }
+        }
+
+        return {
+            directorsIndex,
+            actorsIndex,
+        }
     }
 
     async parse_directors(mediaInfo) {
         let directors = '';
-        const span_list = mediaInfo.children[1].children[2].children;
+        let span_list = null;
+        try {
+            span_list = mediaInfo.children[1].children[2].children;
+        } catch (error) {
+            this.logger.error(error);
+        }
+        if (!span_list) return directors;
         span_list.forEach(span => {
             if (span.type == 'tag') {
                 directors += span.children[0].data;
@@ -37,8 +84,13 @@ class MovieService extends Service {
 
     async parse_actors(mediaInfo) {
         let actors = '';
-        const span_list = mediaInfo.children[7].children[2].children;
-        console.log(span_list)
+        let span_list = null;
+        try {
+            span_list = mediaInfo.children[7].children[2].children;
+        } catch (error) {
+            this.logger.error(error);
+        }
+        if (!span_list) return actors;
         span_list.forEach(span => {
             if (span.type == 'tag') {
                 actors += span.children[0].data;
